cc8534fbaa953b9edf7c8be0c5d20dd75502f53b
İsmail ÇAKMAK <werduet@hotmail.com>
2024-08-11 01:16:04 +0300

    20240811

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+const Homepage = () => {
+  const item = 'Hello';
+  console.log(item.toLowerCase());
+  return <div>Homepage</div>;
+};
+
+export default Homepage;
---
+++ app/ui/dashboard/navbar/navbar.jsx
@@ -0,0 +1,8 @@
+'use client';
+import React from 'react';
+
+const Navbar = () => {
+  return <div className="">Navbar</div>;
+};
+
+export default Navbar;
