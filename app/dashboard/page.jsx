d4d74fcdb28a8f20e9ab945feb2fac23c2ec3be7
İsmail ÇAKMAK <werduet@hotmail.com>
2024-08-13 17:10:09 +0300

    20240813 upgrade

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import styles from '../ui/dashboard/dashboard.module.css';
+import { cards } from '../lib/data';
 import Card from '../ui/dashboard/card/card';
+import Chart from '../ui/dashboard/chart/chart';
+import styles from '../ui/dashboard/dashboard.module.css';
 import Rightbar from '../ui/dashboard/rightbar/rightbar';
 import Transactions from '../ui/dashboard/transactions/transactions';
-import Chart from '../ui/dashboard/chart/chart';
 
 const Dashboard = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.main}>
         <div className={styles.cards}>
-          <Card />
-          <Card />
-          <Card />
+          {cards.map((item) => (
+            <Card item={item} key={item.id} />
+          ))}
         </div>
         <Transactions />
         <Chart />
--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import styles from '@/app/ui/dashboard/products/products.module.css';
-import Pagination from '@/app/ui/dashboard/pagination/pagination';
-import Link from 'next/link';
 import Image from 'next/image';
+import Link from 'next/link';
+import styles from '@/app/ui/dashboard/products/products.module.css';
 import Search from '@/app/ui/dashboard/search/search';
+import Pagination from '@/app/ui/dashboard/pagination/pagination';
+import { fetchProducts } from '@/app/lib/data';
+import { deleteProduct } from '@/app/lib/actions';
 
-const ProductsPage = () => {
+const ProductsPage = async ({ searchParams }) => {
+  const q = searchParams?.q || '';
+  const page = searchParams?.page || 1;
+  const { count, products } = await fetchProducts(q, page);
+
   return (
     <div className={styles.container}>
       <div className={styles.top}>
-        <Search placeholder="Search for a product" />
+        <Search placeholder="Search for a product..." />
         <Link href="/dashboard/products/add">
           <button className={styles.addButton}>Add New</button>
         </Link>
@@ -26,37 +31,40 @@ const ProductsPage = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <div className={styles.product}>
-                <Image
-                  src="/noproduct.jpg"
-                  alt=""
-                  width={40}
-                  height={40}
-                  className={styles.productImage}
-                />
-                Iphone
-              </div>
-            </td>
-            <td>desc</td>
-            <td>$933.33</td>
-            <td>13.02.2022</td>
-            <td>active</td>
-            <td>
-              <div className={styles.buttons}>
-                <Link href="/">
-                  <button className={`${styles.button} ${styles.view}`}>View</button>
-                </Link>
-                <Link href="/">
-                  <button className={`${styles.button} ${styles.delete}`}>Delete</button>
-                </Link>
-              </div>
-            </td>
-          </tr>
+          {products.map((product) => (
+            <tr key={product.id}>
+              <td>
+                <div className={styles.product}>
+                  <Image
+                    src={product.img || '/noproduct.jpg'}
+                    alt=""
+                    width={40}
+                    height={40}
+                    className={styles.productImage}
+                  />
+                  {product.title}
+                </div>
+              </td>
+              <td>{product.desc}</td>
+              <td>${product.price}</td>
+              <td>{product.createdAt?.toString().slice(4, 16)}</td>
+              <td>{product.stock}</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href={`/dashboard/products/${product.id}`}>
+                    <button className={`${styles.button} ${styles.view}`}>View</button>
+                  </Link>
+                  <form action={deleteProduct}>
+                    <input type="hidden" name="id" value={product.id} />
+                    <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+                  </form>
+                </div>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
-      <Pagination />
+      <Pagination count={count} />
     </div>
   );
 };
